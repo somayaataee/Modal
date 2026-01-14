a265deb948df67b2f92832615d1090e0068877c8
Somaya Ataie <somayaatee0@gmail.com>
2026-01-14 12:26:11 +0430

    create js file

---
+++ myscript.js
@@ -0,0 +1,4 @@
+const modal = document.querySelector(".modal");
+const ovarlay = document.querySelector(".ovarlay");
+const btnsOpenmodal = document.querySelectorAll(".show-modal");
+const btnsClosemodal =document.querySelector(".close-modal");
