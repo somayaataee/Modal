71536cdb47eddc5c9b26aca662d0f828df34d097
Somaya Ataie <somayaatee0@gmail.com>
2026-01-14 13:37:05 +0430

    add

--- myscript.js
+++ myscript.js
@@ -2,3 +2,18 @@ const modal = document.querySelector(".modal");
 const ovarlay = document.querySelector(".ovarlay");
 const btnsOpenmodal = document.querySelectorAll(".show-modal");
 const btnsClosemodal =document.querySelector(".close-modal");
+
+// console.log(btnsOpenmodal);
+
+for(let i = 0; i< btnsOpenmodal.length; i++){
+    btnsOpenmodal[i].addEventListener('click',function(){
+        modal.classList.remove('hidden');
+        ovarlay.classList.remove('hidden');
+    });
+    btnsClosemodal.addEventListener('click',function(){
+        modal.classList.add('hidden');
+        ovarlay.classList.add('hidden');
+    });
+    
+    
+}
